38086daf75996729730edc07a75c732dea4ef732
RebelliousUno <RebelliousUno@gmail.com>
2019-11-10 13:11:09 +0000

    feat(): Move files into package structure

--- src/main/kotlin/Main.kt
+++
@@ -1,7 +0,0 @@
-package uno.rebellious.desertgooseforhope
-
-fun main(args: Array<String>) {
-    BotManager.startTwirkForChannel()
-}
-
-data class Channel(val channel: String, val prefix: String, val nick: String, val token: String)
